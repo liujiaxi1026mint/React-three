486f456620b8c2660e6758a319e52ab124d3c61c
刘佳汐 <45161154+liujiaxi1026mint@users.noreply.github.com>
2020-10-24 20:28:03 +0800

    Add files via upload

---
+++ precache-manifest.6bceff157ecf1cea282caeb30392784b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0caff7bebb10375f4e0845adb04a3b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2740a036d946431cf21",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "a4d39096971223f28d25",
+    "url": "/static/css/main.92a3ebff.chunk.css"
+  },
+  {
+    "revision": "c2740a036d946431cf21",
+    "url": "/static/js/2.4a910684.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.4a910684.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4d39096971223f28d25",
+    "url": "/static/js/main.7f0d3730.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
